26d556f619e65b2a6bb0968fecbd7854732da4f6
João Oliveira <jb-oliveira@github.com>
2021-11-24 06:44:50 -0300

    Teste apagar

--- api/migrate.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mvn clean compile flyway:migrate  -Dflyway.user=api_user -Dflyway.password=api_user -Dflyway.url=jdbc:postgresql://localhost:5432/api -Dflyway.driver=org.postgresql.Driver
-
